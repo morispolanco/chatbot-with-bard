05c0d9b9b7164f8884619111625f5dce0cbc8bfa
morispolanco <morispolanco@gmail.com>
2023-12-25 10:21:01 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,47 @@
+```python
 import streamlit as st
 from streamlit_chat import message
-from requests import get
+from google_auth_oauthlib.flow import InstalledAppFlow
+from bs4 import BeautifulSoup
+import requests
+import re
 
-# Función para obtener el precio más bajo de un producto en Guatemala
-def get_lowest_price(product_name):
-    url = "https://api.bard.ai/v1/query"
-    params = {
-        "query": f"precio mas bajo en guatemala de un {product_name}",
-        "language": "es",
-        "model": "PaLM2"
+# Define the BARD API's endpoint and parameters
+bard_endpoint = "https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generateText?key={{API_KEY}}"
+bard_params = {
+    "prompt": {
+        "text": "Find the lowest price for [product name] in Guatemala.",
+        "model_parameters": {
+            "temperature": 0.7
+        }
     }
-    response = get(url, params=params)
-    if response.status_code == 200:
-        data = response.json()
-        return data["result"]["price"]
-    else:
-        return None
+}
 
-# Function to process incoming messages
-def handle_incoming_message(message):
-    product_name = message.text
-    if product_name:
-        price = get_lowest_price(product_name)
-        if price is not None:
-            message.reply(f"El precio más bajo de {product_name} en Guatemala es de Q{price}")
-        else:
-            message.reply(f"No se pudo encontrar el precio de {product_name}")
+# Set up Streamlit components
+st.title("Lowest Price Finder")
+product_name = st.text_input("Product Name:")
+st.write("Searching...")
 
-# Register the message handler
-message.on_message(handle_incoming_message)
+# Use BARD API to find the product's lowest price in Guatemala
+response = requests.post(bard_endpoint, json=bard_params)
+result = response.json()
+price_response = result["candidates"][0]["output"]
 
-# Create the chat interface
-chat = message()
+# Extract the price from the BARD response
+price = re.findall(r"Q(\d+)", price_response)[0]
+
+# Use BeautifulSoup to scrape the product's price from a popular Guatemalan e-commerce site
+url = "https://www.mercadolibre.com.gt/ofertas"
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36"}
+response = requests.get(url, headers=headers)
+soup = BeautifulSoup(response.text, "html.parser")
+
+# Find the product's price on the website
+product_price = soup.find("span", class_="price-tag-amount").text.strip()
+
+# Compare the BARD and website prices and display the lowest one
+if int(price) < int(product_price):
+    message("Lowest price found: Q{}".format(price))
+else:
+    message("Lowest price found on Mercado Libre: {}".format(product_price))
+```
