7d8dca8bb9f17ceb0e881ff6337e016fd2239a16
morispolanco <morispolanco@gmail.com>
2023-12-25 07:51:59 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,40 @@
-from bardapi import Bard
 import streamlit as st
-from streamlit_chat import message
-import os
-
-st.title("KHAI")
-st.subheader("Power by BARD")
-st.caption("Author: Davann Tet")
-st.caption("KHAI")
-
-# Configurar la clave de la API de Bard
-os.environ['_BARD_API_KEY'] = "bAjcVAWV5diEbSb5Mmhk7u1wV06cKjVc5LMCLyWt4xPnIJTMDrULSF__pa-hcgFusHzTpQ."
+import requests
 
 def get_bard_answer(prompt):
-    return Bard().get_answer(prompt)['content']
+    url = "https://api.bard.ai/v1/query"
+    params = {
+        "query": prompt,
+        "language": "es",
+        "model": "PaLM2"
+    }
+
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        data = response.json()
+        return data.get("result", {}).get("content", "No answer")
+    except requests.exceptions.RequestException as e:
+        st.error(f"Error al obtener respuesta de Bard: {e}")
+        return "Error"
 
 def main():
-    if 'qa_history' not in st.session_state:
-        st.session_state['qa_history'] = []
+    st.title("Chatbot con Bard")
+    st.caption("Powered by BARD")
 
-    user_question = st.text_input("What is your question?")
+    if "qa_history" not in st.session_state:
+        st.session_state.qa_history = []
 
-    if user_question:
+    user_question = st.text_input("¿Cuál es tu pregunta?")
+
+    if st.button("Obtener respuesta"):
         bot_answer = get_bard_answer(user_question)
-        st.session_state['qa_history'].insert(0, {'question': user_question, 'answer': bot_answer})
+        st.session_state.qa_history.append({"question": user_question, "answer": bot_answer})
 
-    if st.session_state['qa_history']:
-        for idx, entry in enumerate(st.session_state['qa_history']):
-            message(entry['question'], key=f"{idx}user", is_user=True)
-            message(entry['answer'], key=f"{idx}bot", is_user=False)
+    for entry in st.session_state.qa_history:
+        st.text(f"Pregunta: {entry['question']}")
+        st.text(f"Respuesta: {entry['answer']}")
+        st.markdown("---")
 
 if __name__ == "__main__":
     main()
